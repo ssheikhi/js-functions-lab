67165cc1e49bcc28fcec373cd7d6bb4dfa2a27ee
ssheikhi <Fx.sheikhi@gmail.com>
2024-10-18 00:22:38 +0200

    complete the exersice basicCalCulator

--- app.js
+++ app.js
@@ -165,3 +165,45 @@ function convertTemperature(temperature, scale){
 console.log('Exercise 8 Result:', convertTemperature(25, "C"));
 console.log('Exercise 8 Result:', convertTemperature(77, "F"));
 
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+function basicCalculator(number1, number2, operation){
+    let operationToLowerCase = String(operation).toLowerCase();
+
+    if (operationToLowerCase === 'add') {
+         return number1 + number2;
+    }
+    else if (operationToLowerCase === 'subtract') {
+        return number1 - number2;
+    }
+    else if (operationToLowerCase === 'multiply') {
+        return number1 * number2;
+    }
+    else if (operationToLowerCase === 'divide'){
+        return number1 / number2;
+    }
+    else {
+        return `The operation - ${operationToLowerCase} - is not valid!`
+    }
+}
+
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "blabla"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "add"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "multiply"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "divide"));
+
