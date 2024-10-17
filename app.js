dafd825fe5438702c707745d1a5146b707bcb509
ssheikhi <Fx.sheikhi@gmail.com>
2024-10-17 23:05:22 +0200

    complete the exersice isCharAVowl

--- app.js
+++ app.js
@@ -42,3 +42,26 @@ console.log('isAdult: ', isAdult(17));
 console.log('isAdult: ', isAdult(18));
 console.log('isAdult: ', isAdult(21));
 console.log('isAdult: ', isAdult(-1));
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel (a, e, i, o, u).
+
+Example: isCharAVowel('a') should return true.
+*/
+
+function isCharAVowel(char){
+    let charToLowerCase = String(char).toLowerCase();
+    if (charToLowerCase === 'a' || charToLowerCase === 'e' || charToLowerCase === 'i' || charToLowerCase === 'o' || charToLowerCase === 'u') {
+        return true;
+    }
+    return false;
+}
+
+console.log('isCharAVowel: ', isCharAVowel('a'));
+console.log('isCharAVowel: ', isCharAVowel('b'));
+console.log('isCharAVowel: ', isCharAVowel('A'));
