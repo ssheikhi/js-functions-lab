847e07753d3bf5f0a2c38a6fd79adccd65de891a
ssheikhi <Fx.sheikhi@gmail.com>
2024-10-17 23:39:27 +0200

    complete the exersice generateEmail

--- app.js
+++ app.js
@@ -65,3 +65,18 @@ function isCharAVowel(char){
 console.log('isCharAVowel: ', isCharAVowel('a'));
 console.log('isCharAVowel: ', isCharAVowel('b'));
 console.log('isCharAVowel: ', isCharAVowel('A'));
+
+/*
+Exercise 4: generateEmail()
+
+Create a function named generateEmail. It should take two strings: 
+a name and a domain. It should return a simple email address.
+
+Example: generateEmail('johnsmith', 'example.com') 
+should return 'johnsmith@example.com'.
+
+Complete the exercise in the space below:
+*/
+const generateEmail = (name, domain) => `${name}@${domain}`
+
+console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
